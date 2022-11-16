4d3290eb05363fae87680cb0cea2179278e8216a
Cinnamon-Akuoma <cboyle.george@gmail.com>
2022-11-16 08:09:59 -0800

    Task Done

--- builtin.c
+++ builtin.c
@@ -14,16 +14,16 @@ int _myexit(info_t *info)
 	if (info->argv[1])  /* If there is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
-			if (exitcheck == -1)
-			{
-				info->status = 2;
-				print_error(info, "Illegal number: ");
-				_eputs(info->argv[1]);
-				_eputchar('\n');
-				return (1);
-			}
+		if (exitcheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
 		info->err_num = _erratoi(info->argv[1]);
-			return (-2);
+		return (-2);
 	}
 	info->err_num = -1;
 	return (-2);
@@ -46,11 +46,11 @@ int _mycd(info_t *info)
 	if (!info->argv[1])
 	{
 		dir = _getenv(info, "HOME=");
-			if (!dir)
-				chdir_ret = /* TODO: what should this be? */
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-			else
-				chdir_ret = chdir(dir);
+		else
+			chdir_ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
@@ -62,20 +62,20 @@ int _mycd(info_t *info)
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
 		chdir_ret = /* TODO: what should this be? */
-		chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
 		chdir_ret = chdir(info->argv[1]);
-			if (chdir_ret == -1)
-			{
-				print_error(info, "can't cd to ");
-				_eputs(info->argv[1]), _eputchar('\n');
-			}
-			else
-			{
-				_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-				_setenv(info, "PWD", getcwd(buffer, 1024));
-			}
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
 	return (0);
 }
 
@@ -90,8 +90,8 @@ int _myhelp(info_t *info)
 	char **arg_array;
 
 	arg_array = info->argv;
-		_puts("help call works. Function not yet implemented \n");
-			if (0)
-				_puts(*arg_array); /* temp att_unused workaround */
-				return (0);
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
 }
